e80122e5689ab0924877d6d35286786a0aa0c88e
yunakim <dbssla31@gmail.com>
2022-12-05 20:42:05 +0900

    signup 초기세팅

---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../SignUp/SignUp.scss';
+
+const SignUp = () => {
+  return (
+    <div>
+      <p>회원가입</p>
+      <p>이메일로 가입하기</p>
+      <button>정육각 회원가입 하기</button>
+      <p>
+        정육각 회원이신가요? <a href="www.naver.com">로그인하기</a>
+      </p>
+    </div>
+  );
+};
+
+export default SignUp;
