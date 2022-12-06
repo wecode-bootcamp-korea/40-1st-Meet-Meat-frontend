019982e5454a2ac966a27660624386ace1ac7296
minki3 <86992006+minki3@users.noreply.github.com>
2022-12-06 11:40:20 +0900

    refactor:로그인페이지 수정사항 수정 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Login.scss';
 import { Link } from 'react-router-dom';
+import './Login.scss';
 
 const Login = () => {
   return (
-    <div className="login-area">
+    <div className="login">
       <div className="login-box">
-        <h1>로그인</h1>
-        <form>
-          <span>이메일 로그인</span>
+        <h1 className="login-logo">로그인</h1>
+        <form className="login-form">
+          <span className="email-login">이메일 로그인</span>
           <input
             className="id-box"
             placeholder="아이디(이메일 주소)를 입력하세요"
@@ -17,11 +17,12 @@ const Login = () => {
             className="password-box"
             placeholder="비밀번호를 입력하세요"
           ></input>
-          <button>로그인</button>
+          <button className="login-button">로그인</button>
         </form>
         <div className="sign-up-link">
           <Link to="/signUp">
-            정육각이 처음이신가요?<span> 회원가입하기</span>
+            정육각이 처음이신가요?
+            <span className="signup-link"> 회원가입하기</span>
           </Link>
         </div>
       </div>
