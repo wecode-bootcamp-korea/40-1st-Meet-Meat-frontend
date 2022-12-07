3e274f1b77b952421f4b9501a35301b0d4aad828
yunakim <dbssla31@gmail.com>
2022-12-07 10:26:24 +0900

    회원가입 페이지 레이아웃 구현 완료

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,42 +1,34 @@
 import React from 'react';
-import '../SignUp/SignUp.scss';
+import './SignUp.scss';
 
 const SignUp = () => {
   return (
     <div>
       <div>
-        <p className="signUpTitle">회원가입</p>
+        <p className="signUpTitleName">회원가입</p>
       </div>
-      <div className="signUpContainer">
-        <p className="">가입정보입력</p>
-        <div className="signUpInputBoxContainer">
-          <tr className="signUpInputBox">
-            <th>아이디(이메일주소)</th>
-            <td>
-              <input className="signUpInput" type="text" />
-            </td>
-          </tr>
-          <tr>
-            <th>비밀번호</th>
-            <td>
-              <input className="signUpInput" type="password" />
-            </td>
-          </tr>
-          <tr>
-            <th>비밀번호 확인</th>
-            <td>
-              <input className="signUpInput" type="password" />
-            </td>
-          </tr>
-          <tr>
-            <th>이름</th>
-            <td>
-              <input className="signUpInput" type="text" />
-            </td>
-          </tr>
+      <div className="signUpInputContainer">
+        <p>가입정보 입력</p>
+        <div className="signUpInputContainerBox">
+          <div className="signUpInputBox">
+            <p className="signUpInputName">아이디(이메일 주소)</p>
+            <input className="signUpInputText" type="text" />
+          </div>
+          <div className="signUpInputBox">
+            <p className="signUpInputName">비밀번호</p>
+            <input className="signUpInputText" type="password" />
+          </div>
+          <div className="signUpInputBox">
+            <p className="signUpInputName">비밀번호 확인</p>
+            <input className="signUpInputText" type="password" />
+          </div>
+          <div className="signUpInputLastBox">
+            <p className="signUpInputName">이름</p>
+            <input className="signUpInputText" type="text" />
+          </div>
         </div>
-        <div>
-          <button className="BackBtn">이전으로</button>
+        <div className="signUpButton">
+          <button className="signUpBackBtn">이전으로</button>
           <button className="signUpBtn">가입하기</button>
         </div>
       </div>
