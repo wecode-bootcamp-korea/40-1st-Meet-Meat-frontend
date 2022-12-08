1bd86ed9f963be997421a0214fcc3109f617dccf
ellen10min@gmail.com <ellen10min@gmail.com>
2022-12-08 16:56:16 +0900

    MODIFY: 상품 리스트 레이아웃 정리

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -42,8 +42,11 @@ const ProductList = () => {
         <div className="allProducts">총 100건</div>
         <ul className="selectProductCategory">
           <li className="productCategoryList">추천순</li>
+          <span className="categoryLine">|</span>
           <li className="productCategoryList">신상품순</li>
+          <span className="categoryLine">|</span>
           <li className="productCategoryList">낮은 가격순</li>
+          <span className="categoryLine">|</span>
           <li className="productCategoryList">높은 가격순</li>
         </ul>
       </div>
