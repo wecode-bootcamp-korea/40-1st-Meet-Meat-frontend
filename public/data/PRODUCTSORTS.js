83f164453cbb6b706f8048b492c834c53c1de6f5
ellen10min@gmail.com <ellen10min@gmail.com>
2022-12-11 23:01:49 +0900

    ADD: 상품 카테고리 상수데이터 map 함수 추가

---
+++ public/data/PRODUCTSORTS.js
@@ -0,0 +1,7 @@
+export const PRODUCTSORTS = [
+  { id: 1, name: '전체', link: '/product-page' },
+  { id: 2, name: '돼지', link: '/product-page' },
+  { id: 3, name: '소', link: '/product-page' },
+  { id: 4, name: '닭', link: '/product-page' },
+  { id: 5, name: '오리', link: '/product-page' },
+];
--- src/pages/ProductList/Section/Category/ProductCategory.js
+++ src/pages/ProductList/Section/Category/ProductCategory.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { PRODUCTSORTS } from './data/PRODUCTSORTS.js';
 import FilterCategory from './FilterCategory/FilterCategory';
 import './ProductCategory.scss';
 
 const ProductCategory = () => {
+  const PRODUCTSORTS = [
+    { id: 1, name: '전체', Link: '/product-page' },
+    { id: 2, name: '돼지', Link: '/product-page' },
+    { id: 3, name: '소', Link: '/product-page' },
+    { id: 4, name: '닭', Link: '/product-page' },
+    { id: 5, name: '오리', Link: '/product-page' },
+  ];
+
+  const [categoryId, setCategoryId] = useState();
   return (
     <>
       <ul className="productListTab">
-        <li className="productCategory">
-          <div>돼지</div>
-        </li>
-        <li className="productCategory">
-          <div>소</div>
-        </li>
-        <li className="productCategory">
-          <div>닭</div>
-        </li>
-        <li className="productCategory">
-          <div>오리</div>
-        </li>
+        {PRODUCTSORTS.map(({ id, name, link }) => {
+          return (
+            <li
+              className="productCategory"
+              key={id}
+              onMouseEnter={() => setCategoryId(id)}
+              onMouseLeave={() => setCategoryId()}
+            >
+              <div key={id} onClick={link}>
+                {name}
+              </div>
+            </li>
+          );
+        })}
       </ul>
       <FilterCategory />
     </>
