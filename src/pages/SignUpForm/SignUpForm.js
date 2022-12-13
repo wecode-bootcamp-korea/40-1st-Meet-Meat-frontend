9803a5831c9fb8c6fa4b475180cac23487e75111
minki3 <86992006+minki3@users.noreply.github.com>
2022-12-13 10:08:30 +0900

    feat:회원가입기능

--- src/pages/SignUpForm/SignUpForm.js
+++ src/pages/SignUpForm/SignUpForm.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
 import './SignUpForm.scss';
 
 const SignUpForm = () => {
+  const Submit = () => {
+    fetch('http://10.58.52.125:8000/users/signup', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify({
+        name: userInfo.name,
+        email: userInfo.email,
+        password: userInfo.password,
+        address: userInfo.address,
+      }),
+    })
+      .then(response => response.json())
+      .then(result => console.log(result));
+  };
+
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+    address: '',
+  });
+  const [checkPassword, setCheckPassword] = useState('');
+  const handleLogin = e => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+  const passwordCheck = e => {
+    setCheckPassword(e.target.value);
+  };
+
+  const isVaildEmail = userInfo.email.includes('@');
+  const isVaildPass = userInfo.password.length > 5;
+  const isVaildAdress = userInfo.address.length > 5;
+  const isVaildName = userInfo.name.length > 2;
+
+  const buttonAble =
+    isVaildEmail &&
+    isVaildPass &&
+    isVaildAdress &&
+    isVaildName &&
+    userInfo.password === checkPassword
+      ? false
+      : true;
+
   return (
     <div className="signUp">
       <div>
@@ -12,29 +57,60 @@ const SignUpForm = () => {
         <div className="signUpInputContainerBox">
           <div className="signUpInputBox">
             <p className="signUpInputName">아이디(이메일 주소)</p>
-            <input className="signUpInputText" type="text" />
+            <input
+              className="signUpInputText"
+              type="text"
+              onChange={handleLogin}
+              name="email"
+              value={userInfo.email}
+              placeholder="email 형식"
+            />
           </div>
           <div className="signUpInputBox">
             <p className="signUpInputName">비밀번호</p>
-            <input className="signUpInputText" type="password" />
+            <input
+              className="signUpInputText"
+              type="password"
+              name="password"
+              onChange={handleLogin}
+              placeholder="비밀번호 8자리 이상"
+            />
           </div>
           <div className="signUpInputBox">
             <p className="signUpInputName">비밀번호 확인</p>
-            <input className="signUpInputText" type="password" />
+            <input
+              className="signUpInputText"
+              type="password"
+              onChange={passwordCheck}
+              value={checkPassword}
+              placeholder="비밀번호 8자리 이상"
+            />
           </div>
           <div className="signUpInputBox">
             <p className="signUpInputName">주소</p>
-            <input className="signUpInputText" type="text" />
+            <input
+              className="signUpInputText"
+              type="text"
+              name="address"
+              onChange={handleLogin}
+            />
           </div>
 
           <div className="signUpInputLastBox">
             <p className="signUpInputName">이름</p>
-            <input className="signUpInputText" type="text" />
+            <input
+              className="signUpInputText"
+              type="text"
+              name="name"
+              onChange={handleLogin}
+            />
           </div>
         </div>
         <div className="signUpButton">
           <button className="signUpBackBtn">이전으로</button>
-          <button className="signUpBtn">가입하기</button>
+          <button className="signUpBtn" disabled={buttonAble} onClick={Submit}>
+            가입하기
+          </button>
         </div>
       </div>
     </div>
