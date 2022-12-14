a3c13f589974d5de5890bb87ce34b0cbafd36f46
yunakim <dbssla31@gmail.com>
2022-12-14 19:08:16 +0900

    Add: config 파일 경로 템플릿 작성

--- src/config.js
+++ src/config.js
@@ -1,1 +1,15 @@
-const BASE_URL = 'http://10.58.52.125:8000/users/';
+const BASE_URL = 'http://10.58.52.125:8000/users';
+
+export const API = {
+  PRODUCTLIST: `${BASE_URL}/`,
+  LOGIN: `${BASE_URL}/`,
+  MAIN: `${BASE_URL}/`,
+  MYPAGE: `${BASE_URL}/`,
+  PRODUCTPAGE: `${BASE_URL}/`,
+  SIGNUPFROM: `${BASE_URL}/`,
+  SIGNUP: `${BASE_URL}/`,
+  DELIVERYPAGE: `${BASE_URL}/`,
+  BASKETPAGE: `${BASE_URL}/`,
+  REVIEWPAGE: `${BASE_URL}/`,
+  ORDERPAGE: `${BASE_URL}/`,
+};
