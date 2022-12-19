6ba75e5e317b6d5d0571d333d5e32b32ea717bac
ellen10min@gmail.com <ellen10min@gmail.com>
2022-12-19 16:18:31 +0900

    Fix: 오타 수정

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Banner from './Section/Banner/Banner';
 import ProductCategory from './Section/Category/ProductCategory/ProductCategory';
 import ProductMeatList from './Section/ProductMeatList/ProductMeatList';
@@ -7,13 +7,7 @@ import './ProductList.scss';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  const { categoryId } = useParams(); // 얘야 우리가 한 번이 되어야 집에 가거든?
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const changeCategory = pageId => {
-    searchParams.set('categories', pageId);
-    setSearchParams(searchParams);
-  };
+  const { categoryId } = useParams();
 
   useEffect(() => {
     // fetch(`http://10.58.52.62:3000/products/${categoryId}/list`) //`name=${name}`
@@ -28,19 +22,12 @@ const ProductList = () => {
         setProducts(data);
       });
   }, [categoryId]);
-  console.log(products);
-  // console.log(categoryId);
-  // console.log(products);
-  // console.log(`http://10.58.52.62:8000/products/name/:${categoryId}`);
+
   return (
     <div className="productList">
       <Banner />
 
-      <ProductCategory
-        categoryId={categoryId}
-        changeCategory={changeCategory}
-        data={products}
-      />
+      <ProductCategory categoryId={categoryId} data={products} />
 
       <ProductMeatList data={products} />
     </div>
